fda22607f8385cebbce60942b6667b33cd682549
Thomas SAUSSOL <thomas.saussol@orange.fr>
2020-02-06 16:55:16 +0100

    projet 1 en cours de construction

--- form_user.php
+++ form_user.php
@@ -1,32 +1,113 @@
 <?php
-  include('user.php');
-  if(isset($_POST['register']))
-  {
-    $user = new User;
-    $user->register($_POST['login'], $_POST['password'], $_POST['email'], $_POST['firstname'], $_POST['lastname']);
-  }
+session_start();
+include ('user.php');
+$greg = new User();
+if (isset($_POST['valider'])) {
+    $greg->register($_POST['login'] , $_POST['password'] , $_POST['email'] , $_POST['firstname'] , $_POST['lastname']);
 
- 
+}
 ?>
 
-
-<!DOCTYPE html>
-<html lang="fr" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Formulaire</title>
-  </head>
-  <body>
-    <form action="form_user.php" method="post">
-
-      <label>Login :</label><input type="text" name="login" required>
-      <label>Mot de passe :</label><input type="password" name="password" required>
-      <label>Email:</label><input type="email" name="email" required>
-      <label>Firstname:</label><input type="text" name="firstname" required>
-      <label>Lastname:</label><input type="text" name="lastname" required>
-
-
-      <input class="buttonOK" type="submit" name="register">
+<html>
+<head>
+    <title>classes</title>
+</head>
+<body>
+    <h1>Inscription</h1>
+    <form method="post" action="">
+        <label>Login</label></br>
+        <input type="text" name="login"></br>
+        <label>Mot de passe</label></br>
+        <input type="text" name="password"></br>
+        <label>Mot de passe</label></br>
+        <input type="text" name="password2"></br>
+        <label>Email</label></br>
+        <input type="mail" name="email" ></br>
+        <label>Firstname</label></br>
+        <input type="text" name="firstname"></br>
+        <label>Lastname</label></br>
+        <input type="text" name="lastname"></br>
+        <input type="submit" name="valider">
     </form>
-  </body>
+    <?php
+
+        $greg = new User();
+
+        if (isset($_POST['connexion'])) {
+        $login=$_POST['login'];
+        $password=$_POST['password'];
+            $greg->connect($login , $password);
+
+}
+?>
+
+            <h1>Connexion</h1>
+    <form method="post" action="">
+        <label>Login</label></br>
+        <input type="text" name="login"></br>
+        <label>Mot de passe</label></br>
+        <input type="text" name="password"></br>
+        <input type="submit" name="connexion">
+    </form>
+
+    <?php
+    if (isset($_POST['deconnexion'])) {
+        $greg->disconnect();
+        echo "vous êtes deconecter";
+    }
+    ?>
+            <h1>Déconnexion</h1>
+    <form method="post"><input type="submit" name="deconnexion"></form>
+
+
+
+      <?php
+    if (isset($_POST['effacer'])) {
+        $greg->delete();
+        session_unset();
+        echo "vous êtes supprimé";
+    }
+    ?>
+            <h1>Supprimé l utilisateur</h1>
+    <form method="post"><input type="submit" name="effacer"></form>
+
+    <?php
+    if (isset($_POST['modifier'])) {
+            $login=$_POST['login'];
+            $password= password_hash($_POST["password"], PASSWORD_DEFAULT);
+            $email=$_POST['email'];
+            $firstname=$_POST['firstname'];
+            $lastname=$_POST['lastname'];
+
+
+         $greg->update($login , $password , $email , $firstname , $lastname);
+         echo "Votre profil est modifié";
+
+
+    }
+    ?>
+      <h1>Modifier</h1>
+    <form method="post" action="">
+        <label>Login</label></br>
+        <input type="text" name="login"></br>
+        <label>Mot de passe</label></br>
+        <input type="text" name="password"></br>
+        <label>Email</label></br>
+        <input type="mail" name="email" ></br>
+        <label>Firstname</label></br>
+        <input type="text" name="firstname"></br>
+        <label>Lastname</label></br>
+        <input type="text" name="lastname"></br>
+        <input type="submit" name="modifier">
+    </form>
+
+
+    <?php
+    if ($_SESSION['login']) {
+         echo $greg->isConnected();
+
+     }
+    ?>
+
+</body>
 </html>
--- user.php
+++ user.php
@@ -1,67 +1,120 @@
 <?php
-class User
-{
 
-  private $id=0; //Définition d'une variable attribut privée ID
-  public $login="";
-  public $email="";
-  public $firstname="";
-  public $lastname=""; 
+class User {
+    private $id = '';
+    public $login = '';
+    public $email = '';
+    public $firstname = '';
+    public $lastname = '';
 
-  public function register($login, $password, $email, $firstname, $lastname)
-  {
-    $db = mysqli_connect('localhost', 'root', '', 'utilisateurs');
-    $login = mysqli_real_escape_string($db, $login);
-    $password = password_hash(mysqli_real_escape_string($db, $password), PASSWORD_BCRYPT);
-    $email = mysqli_real_escape_string($db, $email);
-    $firstname = mysqli_real_escape_string($db, $firstname);
-    $lastname = mysqli_real_escape_string($db, $lastname);
-    $error = mysqli_query($db,"INSERT INTO users (login, password, email, firstname, lastname) VALUES ('$login','$password', '$email', '$firstname', '$lastname')");
-    var_dump($error);
-  }
-  public function connect($login, $password)
-  {
 
-  }
-  public function disconnect(){
+    public function register($login,$password,$email,$firstname,$lastname)
+    {
 
-  }
-  public function delete()
-  {
+        $connexion =  mysqli_connect("localhost","root","","classe");
+        $password= password_hash($password, PASSWORD_DEFAULT);
+        $requet="SELECT * FROM user WHERE login='".$login."'";
+        $query2=mysqli_query($connexion,$requet);
+        $resultat=mysqli_fetch_all($query2);
+        $trouve=false;
 
-  }
-  public function update($login, $email, $firstname, $lastname)
-  {
 
-  }
-  public function isConnected()
-  {
+        if(!empty($resultat[0]))
+        {
+           $trouve=true;
+           echo "<p class='erreur'><b>Login déja existant!!</b></p>";
+        }
 
-  }
-  public function getAllInfos()
-  {
+       if ($trouve==false)
+       {
+            $sql = "INSERT INTO user (`id`, `login`, `password`, `email`, `firstname`, `lastname`) VALUES (NULL, '".$login."','".$password."','".$email."','".$firstname."','".$lastname."')";
 
-  }
-  public function getLogin()
-  {
+            if(mysqli_query($connexion,$sql))
+           {
+                return array($login, $password, $email, $firstname, $lastname);
+            }
+        }
 
-  }
-  public function getEmail()
-  {
+       else
+       {
+          echo "<p class='erreur'><b>Les mots de passe doivent être identique!</b></p>";
+       }
+    }
 
-  }
-  public function getFirstname()
-  {
+    public function connect($login, $password){
 
-  }
-  public function getLastname()
-  {
+        if (!isset($_SESSION['login']))
+    {
+            if(isset($_POST['login']) && isset($_POST['password']))
+        {
+                $connexion = mysqli_connect ("localhost", "root", "", "classe");
+                $login = mysqli_real_escape_string($connexion,htmlspecialchars($_POST['login']));
+                $password = mysqli_real_escape_string($connexion,htmlspecialchars($_POST['password']));
 
-  }
-  public function refresh()
-  {
+            if($login !== "" && $password !== "")
+            {
+                $requete = "SELECT count(*) FROM user WHERE login = '".$login."' ";
+                $exec_requete = mysqli_query($connexion,$requete);
+                $reponse      = mysqli_fetch_array($exec_requete);
+                $count = $reponse['count(*)'];
 
-  }
+                $requete4 = "SELECT * FROM user WHERE login='".$login."'";
+                $exec_requete4 = mysqli_query($connexion,$requete4);
+                $reponse4 = mysqli_fetch_array($exec_requete4);
+
+                echo "vous etes bien connecté";
+                if( $count!=0 && $login !== "" && password_verify($password, $reponse4[2]) )
+                {
+
+                $_SESSION['login'] = $_POST['login'];
+                $utilisateurs = $_SESSION['login'];
+                }
+                else
+                {
+                header('Location: index.php?erreur=1'); // utilisateur ou mot de passe incorrect
+                }
+            }
+
+        }
+
+    }
+}
+
+    public function disconnect(){
+           session_unset();
+        }
+
+    public function delete(){
+      $connexion = mysqli_connect ("localhost", "root", "", "classe");
+      $requete = "DELETE  FROM user WHERE login='".$_SESSION['login']."'";
+      $query = mysqli_query($connexion,$requete);
+
+    }
+    public function update($login, $password, $email, $firstname,$lastname){
+         $connexion = mysqli_connect ("localhost", "root", "", "classe");
+          $password= password_hash($_POST["password"],PASSWORD_DEFAULT);
+         $requete = "UPDATE user SET login = '$login', password = '$password', email = '$email', firstname = '$firstname', lastname = '$lastname' WHERE login = '".$_SESSION['login']."'";
+         var_dump($requete);
+         $query = mysqli_query($connexion,$requete);
+         var_dump($query);
+         $_SESSION['login'] = $login;
+         header('Location: index.php');
+
+    }
+
+
+     public function isConnected(){
+     	if (isset($_SESSION['login']))
+     		return true;
+
+     	else{
+     		echo "vous devez vous connecter";
+     		return false;
+
+     	}
+}
 
 }
- ?>
+
+
+?>
